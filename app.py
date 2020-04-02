b13a6ad83338ec5d2ccd5cc9d315b0b575402759
arvindspal <arvindpal.pal@gmail.com>
2020-04-02 23:16:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ from test import Test
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/predict", methods=['POST'])
 def predict():
     test = Test()
     return test.predict()
+
+
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
